512f8b4bec78d52a4daa28d38093b68256b6cd3d
Gitjunk1 <mr.juse52@gmail.com>
2022-01-31 19:16:33 +0300

    test commit

--- functions.php
+++ functions.php
@@ -7,7 +7,8 @@ function arch_styles() {
 	wp_enqueue_style( 'normolize-style', get_template_directory_uri() . '/assets/css/normolize.css' );
 
 
-	wp_enqueue_style( 'main-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'main-style', get_stylesheet_uri() ); //hello//
+	
 
 // 	wp_enqueue_script( 'script-name', get_template_directory_uri() . '/js/example.js', array(), '1.0.0', true );
 }
